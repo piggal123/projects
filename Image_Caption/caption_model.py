76849b58f97a0496ed7f19088f3e38b2008d90fc
piggal123 <116363425+piggal123@users.noreply.github.com>
2024-05-05 10:02:06 +0300

    Update caption_model.py

--- Image_Caption/caption_model.py
+++ Image_Caption/caption_model.py
@@ -4,11 +4,18 @@ from PIL import Image
 
 class CaptionModel:
 
-    def __init__(self, model_name):
+    def __init__(self, model_name: str):
         self.processor = BlipProcessor.from_pretrained(model_name)
         self.model = BlipForConditionalGeneration.from_pretrained(model_name)
 
-    def caption_image(self, file_path) -> str:
+    def caption_image(self, file_path: str) -> str:
+        """
+        generating a caption for the image, using the model
+        args:
+            :param file_path str: the file path
+        return:
+        str: the image caption
+        """
         raw_image = Image.open(file_path)
 
         inputs = self.processor(raw_image, return_tensors="pt")
